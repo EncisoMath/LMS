cc67985c0e287787bffd572da614e42c2b96d399
EncisoMath <enciso.math@gmail.com>
2026-06-12 22:17:26 -0500

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = '0.21.0';
+  const APP_VERSION = '0.22.0';
   const DATA_FILES = {
     users: './data/users.json',
     assignments: './data/assignments.json',
