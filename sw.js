288784c4da691ee5eb8297e089e072afc487b5ab
EncisoMath <enciso.math@gmail.com>
2026-08-24 19:25:16 -0500

    .

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('./background-content-sync.js');
 
-const SW_VERSION = 'encisomath-offline-v0.25.107';
+const SW_VERSION = 'encisomath-offline-v0.25.108';
 const APP_CACHE = `${SW_VERSION}-app`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
 const EXTERNAL_CACHE = `${SW_VERSION}-external`;
